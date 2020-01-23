a36fb91d0accbcb9be46ff971b95ab5a4dab3ded
vinirafa2019 <vinirafa2013@hotmail.com>
2020-01-23 02:58:58 -0300

    exemplo1

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+}
